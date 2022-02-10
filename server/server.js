82174ffb397f8dbb42e12f7f980d375e82534511
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2022-02-10 18:29:54 +0100

    connect to mongoDB

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+connectDB();
 const app = express();
 
 app.use(express.json());
