07590105e651d3f6d6e7c8eba5f91294cf76c86d
Alexander Rusiecki <54362381+alexander-rusiecki@users.noreply.github.com>
2022-02-10 16:33:10 +0100

    add .env and sets up express server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,8 @@
-console.log('yo');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.listen(port, () =>
+  console.log(`server running on port: ${process.env.PORT}`)
+);
